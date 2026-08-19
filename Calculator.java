400e101f89042be60d9354f2fb7a1290772f09e9
Aman Barbate <148617560+AmanBarbate@users.noreply.github.com>
2026-08-19 14:45:44 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,103 +1,168 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
-public class Calculator {
-
-    static void add(int a, int b) {
-        System.out.println("Result = " + (a + b));
+  
+class Main{
+    static int add(int a, int b){
+      return a+b;
+    }
+    static int sub(int a, int b){
+      return a-b;
+    }
+static int mul(int a, int b){
+      return a*b;
     }
 
-    static void subtract(int a, int b) {
-        System.out.println("Result = " + (a - b));
+static double div(int a, int b){
+    if(b == 0){
+        System.out.println("Cannot divide by zero.");
+        return 0;
     }
 
-    static void multiply(int a, int b) {
-        System.out.println("Result = " + (a * b));
+
+    return (double)a / b;
+}
+
+    static int max(int a, int b){
+      
+      if(a>=b){
+        return a;
+      } else{
+        return b;
+      }
     }
+    
+    static int min(int a, int b){
+      
+      if(a<=b){
+        return a;
+      } else{
+        return b;
+      }
 
-    static void divide(int a, int b) {
-        System.out.println("Result = " + (a / b));
     }
+    static double currency(double rs){
+    return rs * 95.75;
+}
 
-    static void remainder(int a, int b) {
-        System.out.println("Result = " + (a % b));
-    }
+static boolean isEven(int n){
+    return n % 2 == 0;
+}
+    
+      
 
-    public static void main(String[] args) {
+    public static void main (String arg[]){
+      Scanner sc=new Scanner(System.in);
+    while(true){
+      System.out.println("Welcome to Calculator");
+   
 
-        Scanner sc = new Scanner(System.in);
-        int choice;
 
-        do {
+System.out.println("1. Add");
+  System.out.println("2. Sub");
+System.out.println("3. Mul");
+System.out.println("4. Div");
+   System.out.println("5. max value");
+   System.out.println("6. min value");
+   System.out.println("7. currency convert"); 
+   System.out.println("8. even odd find"); 
+System.out.println("9. Exit");
 
-            System.out.println("\n========== JAVA CALCULATOR ==========");
-            System.out.println("1. Addition");
-            System.out.println("2. Subtraction");
-            System.out.println("3. Multiplication");
-            System.out.println("4. Division");
-            System.out.println("5. Remainder");
-            System.out.println("6. Exit");
-            System.out.print("Enter your choice: ");
+System.out.println("ENTER YOUR CHOICE");
+int choice=sc.nextInt();
 
-            try {
 
-                choice = sc.nextInt();
 
-                if (choice == 6) {
-                    System.out.println("Thank you for using Calculator!");
+
+
+switch(choice){
+
+  case 1:
+                    System.out.print("Enter First Number: ");
+                    int a1 = sc.nextInt();
+                    System.out.print("Enter Second Number: ");
+                    int b1 = sc.nextInt();
+
+                    System.out.println("Answer = " + add(a1, b1));
                     break;
-                }
 
-                if (choice < 1 || choice > 6) {
-                    System.out.println("Invalid Choice!");
-                    continue;
-                }
+                case 2:
+                    System.out.print("Enter First Number: ");
+                    int a2 = sc.nextInt();
+                    System.out.print("Enter Second Number: ");
+                    int b2 = sc.nextInt();
 
-                System.out.print("Enter first number: ");
-                int num1 = sc.nextInt();
+                    System.out.println("Answer = " + sub(a2, b2));
+                    break;
 
-                System.out.print("Enter second number: ");
-                int num2 = sc.nextInt();
+                case 3:
+                    System.out.print("Enter First Number: ");
+                    int a3 = sc.nextInt();
+                    System.out.print("Enter Second Number: ");
+                    int b3 = sc.nextInt();
 
-                switch (choice) {
+                    System.out.println("Answer = " + mul(a3, b3));
+                    break;
 
-                    case 1:
-                        add(num1, num2);
-                        break;
+                case 4:
+                    System.out.print("Enter First Number: ");
+                    int a4 = sc.nextInt();
+                    System.out.print("Enter Second Number: ");
+                    int b4 = sc.nextInt();
 
-                    case 2:
-                        subtract(num1, num2);
-                        break;
+                    if (b4 == 0) {
+                        System.out.println("Cannot divide by zero.");
+                    } else {
+                        System.out.println("Answer = " + div(a4, b4));
+                    }
+                    break;
 
-                    case 3:
-                        multiply(num1, num2);
-                        break;
+                case 5:
+                    System.out.print("Enter First Number: ");
+                    int a5 = sc.nextInt();
+                    System.out.print("Enter Second Number: ");
+                    int b5 = sc.nextInt();
 
-                    case 4:
-                        divide(num1, num2);
-                        break;
+                    System.out.println("Maximum = " + max(a5, b5));
+                    break;
 
-                    case 5:
-                        remainder(num1, num2);
-                        break;
-                }
+                case 6:
+                    System.out.print("Enter First Number: ");
+                    int a6 = sc.nextInt();
+                    System.out.print("Enter Second Number: ");
+                    int b6 = sc.nextInt();
 
-            } catch (ArithmeticException e) {
+                    System.out.println("Minimum = " + min(a6, b6));
+                    break;
 
-                System.out.println("Error: Cannot divide by zero.");
+                case 7:
+                System.out.print("Enter Rupees: ");
+                    double rs = sc.nextDouble();
 
-            } catch (InputMismatchException e) {
+                    System.out.println("USD = " + currency(rs));
+                    break;
+         
+                    
 
-                System.out.println("Error: Please enter valid integer numbers.");
-                sc.nextLine(); // Clear invalid input
+                case 8:
+                    System.out.println("Enter no");
+                     int eo=sc.nextInt();
+                      if(eo % 2==0){
+                         System.out.println("EVEN");
+                          } else { System.out.println("ODD");
+                           } 
+                           break;
 
-            } finally {
+                case 9:
+                    System.out.println("Thanks for Visiting.");
+                    return;
 
-                System.out.println("------------------------------------");
-            }
+                default:
+                  System.out.println("Invalid Choice");
+  
+}
 
-        } while (true);
+    }
 
-        sc.close();
     }
 }
+
+
